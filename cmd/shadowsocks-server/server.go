a29ec381e0e7a9aa84d0a6b9bbfd6d81fd5da626
songwenbin <songwenbin@outlook.com>
2018-10-05 22:36:25 +0800

    compile error, because import error code library

--- cmd/shadowsocks-server/server.go
+++ cmd/shadowsocks-server/server.go
@@ -19,7 +19,7 @@ import (
 	"syscall"
 	"time"
 
-	ss "github.com/shadowsocks/shadowsocks-go/shadowsocks"
+	ss "github.com/ss-go-witheos/shadowsocks"
 
 	"github.com/ss-go-witheos/eosapi"
 	"github.com/ss-go-witheos/httpserver"
